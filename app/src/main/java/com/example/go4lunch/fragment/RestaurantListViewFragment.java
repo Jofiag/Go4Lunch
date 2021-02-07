318c8eb1315881dfb60d6ccd9f12f5bdc1261944
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-02-07 18:52:28 +0100

    Setting recyclerView on both RestaurantListViewFragment and WorkmateListViewFragment.

--- app/src/main/java/com/example/go4lunch/fragment/RestaurantListViewFragment.java
+++ app/src/main/java/com/example/go4lunch/fragment/RestaurantListViewFragment.java
@@ -1,14 +1,23 @@
 package com.example.go4lunch.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.go4lunch.R;
+import com.example.go4lunch.adapter.RestaurantRecyclerViewAdapter;
+import com.example.go4lunch.model.Restaurant;
+import com.example.go4lunch.model.Workmate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestaurantListViewFragment extends Fragment {
 
@@ -28,7 +37,16 @@ public class RestaurantListViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_restaurant_list_view, container, false);
 
+        RecyclerView recyclerView = view.findViewById(R.id.restaurant_list_recycler_view);
 
+        List<Restaurant> restaurantList = new ArrayList<>();
+        Context context = view.getContext();
+
+        RestaurantRecyclerViewAdapter restaurantAdapter = new RestaurantRecyclerViewAdapter(context, restaurantList);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(restaurantAdapter);
 
         return view;
     }
--- app/src/main/java/com/example/go4lunch/fragment/WorkmateListViewFragment.java
+++ app/src/main/java/com/example/go4lunch/fragment/WorkmateListViewFragment.java
@@ -1,14 +1,24 @@
 package com.example.go4lunch.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.go4lunch.R;
+import com.example.go4lunch.adapter.RestaurantRecyclerViewAdapter;
+import com.example.go4lunch.adapter.WorkmateRecyclerViewAdapter;
+import com.example.go4lunch.model.Restaurant;
+import com.example.go4lunch.model.Workmate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkmateListViewFragment extends Fragment {
 
@@ -28,7 +38,16 @@ public class WorkmateListViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_workmate_list_view, container, false);
 
+        RecyclerView recyclerView = view.findViewById(R.id.workmate_list_recycler_view);
 
+        List<Workmate> workmateList = new ArrayList<>();
+        Context context = view.getContext();
+
+        WorkmateRecyclerViewAdapter workmateAdapter = new WorkmateRecyclerViewAdapter(context, workmateList);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(workmateAdapter);
 
         return view;
     }
