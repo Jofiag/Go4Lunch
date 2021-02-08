760f19a136755b5fdc610e960d125a1ff15a735b
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-02-08 09:51:47 +0100

    Referencing layout item in the activity (RestaurantDetailsActivity) and set the recycler view.

--- app/src/main/java/com/example/go4lunch/controller/RestaurantDetailsActivity.java
+++ app/src/main/java/com/example/go4lunch/controller/RestaurantDetailsActivity.java
@@ -1,35 +1,83 @@
 package com.example.go4lunch.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.go4lunch.R;
+import com.example.go4lunch.adapter.RestaurantRecyclerViewAdapter;
+import com.example.go4lunch.adapter.WorkmateRecyclerViewAdapter;
 import com.example.go4lunch.model.Restaurant;
 import com.example.go4lunch.model.Workmate;
 import com.example.go4lunch.util.Constants;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RestaurantDetailsActivity extends AppCompatActivity {
+    private ImageView yellowStar;
+    private ImageView callImageView;
+    private ImageView starImageView;
+    private ImageView globeImageView;
+    private FloatingActionButton fab;
+    private RecyclerView recyclerView;
+    private ImageView restaurantImageView;
+    private TextView restaurantNameTextView;
+    private TextView foodCountryAndRestaurantAddress;
+
     private Restaurant restaurant;
-    private Workmate workmate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_details);
 
+        setReferences();
+        restaurant = getRestaurantSelected();
+        setRecyclerView();
+
+    }
+
+    private void setReferences() {
+        fab = findViewById(R.id.fab);
+        yellowStar = findViewById(R.id.yellow_star);
+        globeImageView = findViewById(R.id.globe_image_view);
+        starImageView = findViewById(R.id.green_star_image_view);
+        callImageView = findViewById(R.id.green_call_image_view);
+        recyclerView = findViewById(R.id.restaurant_details_recycler_view);
+        restaurantImageView = findViewById(R.id.restaurant_image_view_details);
+        restaurantNameTextView = findViewById(R.id.restaurant_name_text_view_details);
+        foodCountryAndRestaurantAddress = findViewById(R.id.food_country_and_restaurant_address_details);
+    }
+
+    private Restaurant getRestaurantSelected(){
+        Restaurant restaurantSelected = new Restaurant();
+        Workmate workmate;
+
         Bundle bundle = getIntent().getExtras();
 
         if (bundle != null) {
             if (bundle.getSerializable(Constants.RESTAURANT_SELECTED_CODE) != null)
-                restaurant = (Restaurant) bundle.getSerializable(Constants.RESTAURANT_SELECTED_CODE);
+                restaurantSelected = (Restaurant) bundle.getSerializable(Constants.RESTAURANT_SELECTED_CODE);
 
             if (bundle.getSerializable(Constants.WORKMATE_SELECTED_CODE) != null) {
                 workmate = (Workmate) bundle.getSerializable(Constants.WORKMATE_SELECTED_CODE);
-                restaurant = workmate.getRestaurantChosen();
+                restaurantSelected = workmate.getRestaurantChosen();
             }
         }
 
-
+        return restaurantSelected;
     }
+
+    private void setRecyclerView(){
+        WorkmateRecyclerViewAdapter workmateAdapter = new WorkmateRecyclerViewAdapter(RestaurantDetailsActivity.this, restaurant.getWorkmateList());
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(RestaurantDetailsActivity.this));
+        recyclerView.setAdapter(workmateAdapter);
+    }
+
+
 }
