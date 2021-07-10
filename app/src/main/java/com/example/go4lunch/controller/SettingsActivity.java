05b67668258e41c681f061d93ceb28784fadd35c
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-07-10 22:04:30 +0200

    Add declaring manage notification function in SettingsActivity.

--- app/src/main/java/com/example/go4lunch/controller/SettingsActivity.java
+++ app/src/main/java/com/example/go4lunch/controller/SettingsActivity.java
@@ -3,14 +3,37 @@ package com.example.go4lunch.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.go4lunch.R;
 
 public class SettingsActivity extends AppCompatActivity {
+    private TextView notificationsOnTextView;
+    private TextView notificationsOffTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        setReferences();
+
+        manageNotifications();
+    }
+
+    private void setReferences() {
+        notificationsOnTextView = findViewById(R.id.ntf_on_text_view);
+        notificationsOffTextView = findViewById(R.id.ntf_off_text_view);
+    }
+
+    private void manageNotifications(){
+        notificationsOnTextView.setOnClickListener(v -> {
+            //TODO: turn off notifications
+        });
+
+        notificationsOffTextView.setOnClickListener(v -> {
+            //TODO: turn off notifications
+        });
     }
 }
