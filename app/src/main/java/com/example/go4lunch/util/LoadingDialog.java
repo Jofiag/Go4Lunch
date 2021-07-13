e73dc6293d415b3dee4133c0b3759dcc27d73b46
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-07-13 16:05:41 +0200

    Create a loading dialog java file that we're going to use to shows the progress bar.

---
+++ app/src/main/java/com/example/go4lunch/util/LoadingDialog.java
@@ -0,0 +1,51 @@
+package com.example.go4lunch.util;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import com.example.go4lunch.R;
+
+public class LoadingDialog {
+    private LoadingDialog INSTANCE;
+
+    private final Activity activity;
+    private final AlertDialog.Builder builder;
+    private AlertDialog alertDialog;
+
+    public LoadingDialog(Activity activity) {
+        this.activity = activity;
+        this.builder = new AlertDialog.Builder(activity);
+
+        initializeDialogBuilder();
+    }
+
+    public LoadingDialog getInstance(Activity activity){
+        if (INSTANCE == null)
+            INSTANCE = new LoadingDialog(activity);
+
+        return INSTANCE;
+    }
+
+    @SuppressLint("InflateParams")
+    private void initializeDialogBuilder(){
+        LayoutInflater inflater = activity.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.loading_dialog, null));
+
+        setDialogCancelable(false);
+    }
+
+    public void setDialogCancelable(boolean isCancelable){
+        builder.setCancelable(isCancelable);
+        alertDialog = builder.create();
+    }
+
+    public void startLoadingDialog(){
+        alertDialog.show();
+    }
+
+    public void dismissLoadingDialog(){
+        alertDialog.dismiss();
+    }
+}
